ae24f39a1302c808f81fca7f85171d280a14ee64
Sarah Ransohoff <sarah.ransohoff@venmo.com>
2016-06-26 20:31:42 -0400

    Refactor routes.

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import AddConversation from './components/add_conversation';
@@ -8,10 +8,10 @@ import Contact from './components/contacts/contact';
 
 const routes = [
   <Route path="/" component={App}>
+    <IndexRoute component={AddConversation} />
+    <Route path="add" component={AddConversation} />
     <Route path="contacts" component={Contacts} />
     <Route path="contacts/:name" component={Contact} />
-    <Route path="add" component={AddConversation} />
-    <Route path="*" component={AddConversation} />
   </Route>
 ];
 
--- server/render_html.js
+++ server/render_html.js
@@ -11,56 +11,11 @@ import createLocation from 'history/lib/createLocation';
 import routes from '../routes';
 
 import App from '../components/app';
-import reducerActions from '../redux/actions';
+import reducerActions, { initialState } from '../redux/actions';
 
 export default function renderHTML(req, res) {
   // grab the state
-  const state = {
-    pendingConversation: {
-      contact: {},
-      conversation: {},
-      isAdding: false
-    },
-    contacts: [
-      {
-        firstName: 'xavier',
-        lastName: 'lopez',
-        conversations: [
-          { date: '12/12/15', method: 'phone' },
-          { date: '09/13/15', method: 'irl' },
-        ]
-      },
-      {
-        firstName: 'ricardo',
-        lastName: 'vazquez',
-        conversations: [
-          { date: '05/18/15', method: 'phone' },
-        ]
-      },
-      {
-        firstName: 'margo',
-        lastName: 'smith',
-        conversations: [
-          { date: '12/02/15', method: 'gchat' },
-          { date: '09/20/14', method: 'gchat' },
-          { date: '10/07/15', method: 'email' },
-          { date: '12/02/15', method: 'slack' },
-        ]
-      },
-      {
-        firstName: 'lindsay',
-        lastName: 'levine',
-        conversations: [
-          { date: '10/19/15', method: 'phone' },
-          { date: '02/22/13', method: 'irl' },
-        ]
-      },
-    ],
-    user: {
-      firstName: 'sarah',
-      lastName: 'ransohoff',
-    }
-  };
+  const state = initialState;
 
   // create the store
   const reducers = {
